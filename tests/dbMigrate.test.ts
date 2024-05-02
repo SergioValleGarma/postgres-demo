cae64e99695e94bfcbb67f9bfbd030166b97abb8
sergio_valle <sergiovallegarma@gmail.com>
2024-05-01 22:47:03 -0500

    clase terminada

--- tests/dbMigrate.test.ts
+++ tests/dbMigrate.test.ts
@@ -1,8 +1,9 @@
 import { describe, test } from "vitest";
 import { migrateDB } from "../src/dbMigrate";
 
+///esta prueba no deberia estar
 describe("Pruebas para generar migration", () => {
-    test("debe de retornar mas de un usuario", async () => {
+    test.skip("debe de retornar mas de un usuario", async () => {
         await migrateDB();
     });
 });
